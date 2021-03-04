adf55e4e3b22c332dd28f9bc674f7f297ec7ebab
Lori Segovia <segovialori@gmail.com>
2021-03-04 15:28:28 -0600

    prepare updated with new functions

--- prepare.py
+++ prepare.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import acquire
+from sklearn.preprocessing import LabelEncoder
 
 def clean_iris(df):
     '''
@@ -13,8 +14,7 @@ def clean_iris(df):
     
     return: clean dataframe
     '''
-    df.drop_duplicates(inplace=True)
-    df.drop(['species_id'], axis=1)
+    df.drop(['species_id'], axis=1, inplace=True)
     df.rename(columns={"species_name": "species"}, inplace=True)
     dummies = pd.get_dummies(df[['species']], drop_first=True)
     return pd.concat([df, dummies], axis=1)
@@ -35,3 +35,52 @@ def prep_iris(df): #when we are translating to its own script, call it as an arg
     train_validate, test = train_test_split(df, test_size=0.2, random_state=123, stratify=df.species)
     train, validate = train_test_split(train_validate, train_size=0.3, random_state=123, stratify=train_validate.species)
     return train, validate, test
+###################### Train, Validate, Test, Split ######################
+def train_validate_test_split(df, seed=123):
+    train_and_validate, test = train_test_split(
+        df, test_size=0.2, random_state=seed, stratify=df.species
+    )
+    train, validate = train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=seed,
+        stratify=train_and_validate.species,
+    )
+    return train, validate, test
+
+
+def split(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.3, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(df, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
+
+def handle_missing_values(df):
+    return df.assign(
+        embark_town=df.embark_town.fillna('Other'),
+        embarked=df.embarked.fillna('O'),
+    )
+
+def remove_columns(df):
+    return df.drop(columns=['deck'])
+
+def encode_embarked(df):
+    encoder = LabelEncoder()
+    encoder.fit(df.embarked)
+    return df.assign(embarked_encode = encoder.transform(df.embarked))
+
+def prep_titanic_data(df):
+    df = df\
+        .pipe(handle_missing_values)\
+        .pipe(remove_columns)\
+        .pipe(encode_embarked)
+    return df
